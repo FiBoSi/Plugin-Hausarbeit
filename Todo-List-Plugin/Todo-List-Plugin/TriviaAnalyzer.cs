54f9946618bd949dc5fd203552e07c8169c66937
FiBoSi <just_heavy@gmx.de>
2017-09-19 13:31:30 +0200

    Revert "Revert "Revert "changes"""
    This reverts commit ddfe2879ba0a68ea250b63571f48fbae36b040cd.

--- Todo-List-Plugin/Todo-List-Plugin/TriviaAnalyzer.cs
+++ Todo-List-Plugin/Todo-List-Plugin/TriviaAnalyzer.cs
@@ -1,4 +1,4 @@
-using System;
+ï»¿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -8,69 +8,52 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace Analyzer1
+namespace Todo_List_Plugin
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class Analyzer1Analyzer : DiagnosticAnalyzer 
+    public class TriviaAnalyzer : DiagnosticAnalyzer
     {
-        // Sammlung von zu erkennenden todo-Schreibweisen
-        private string[] todoList = { "todo", "Todo", "TODO" };
-        // Sammlung der gefundenen Todos
-        public string[] todoFound;
-
         public const string DiagnosticId = "TriviaAnalyzer";
         internal static readonly LocalizableString Title = "TriviaAnalyzer Title";
-        internal static readonly LocalizableString MessageFormat = "TriviaAnalyzer";
+        internal static readonly LocalizableString MessageFormat = "TriviaAnalyzer '{0}'";
         internal const string Category = "TriviaAnalyzer Category";
 
         internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
-        // Zugriff auf Syntax-Baum
         public override void Initialize(AnalysisContext context)
         {
             context.RegisterSyntaxTreeAction(this.HandleSyntaxTree);
-            
         }
-        
+
         private void HandleSyntaxTree(SyntaxTreeAnalysisContext context)
         {
             SyntaxNode root = context.Tree.GetCompilationUnitRoot(context.CancellationToken);
-
-            // Sammeln aller Single- und MultiLineCommentTrivia
             var commentNodes = from node in root.DescendantTrivia() where node.IsKind(SyntaxKind.MultiLineCommentTrivia) || node.IsKind(SyntaxKind.SingleLineCommentTrivia) select node;
 
-            if (!commentNodes.Any())
+            if(!commentNodes.Any())
             {
                 return;
             }
-
-            //int counter = 0;
-            foreach (var node in commentNodes)
+            foreach(var node in commentNodes)
             {
                 string commentText = "";
-                // Ablegen des node-Inhalts in commentText als String
-                switch (node.Kind())
+                switch(node.Kind())
                 {
                     case SyntaxKind.SingleLineCommentTrivia:
                         commentText = node.ToString().TrimStart('/');
                         break;
                     case SyntaxKind.MultiLineCommentTrivia:
-                        var nodeText = node.ToString().TrimStart('/');
-                        commentText = nodeText.Substring(0, nodeText.Length - 4);
+                        var nodeText = node.ToString();
+                        commentText = nodeText.Substring(2, nodeText.Length - 4);
                         break;
                 }
-                
-                // Prüfen ob commentText eine bekannte todo-Schreibweise enthält
-                if(MyContains.MyOwnContains(commentText, todoList))
+
+                if(!string.IsNullOrEmpty(commentText))
                 {
-                    // erzeugen einer Diagnostic an der Location des nodes
                     var diagnostic = Diagnostic.Create(Rule, node.GetLocation());
                     context.ReportDiagnostic(diagnostic);
-
-                    ClassLibrary1.Class1.todos.Add(commentText);
-                    //counter++;
                 }
             }
         }
